1724fc1bd128cb9757de9848863a53ff70150f5a
hyperpolymath <6759885+hyperpolymath@users.noreply.github.com>
2026-06-07 03:21:31 +0100

    chore(android): resolve rebase conflicts by restoring JNI shims over deleted tree

---
+++ android/build.gradle.kts
@@ -0,0 +1,22 @@
+// Top-level build file for NeuroPhone Android App
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
